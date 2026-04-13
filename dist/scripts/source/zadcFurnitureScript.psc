IHateMyKite/DeviousDevicesNG

commit 09eee90670b92bc5d44dcbd8bbe1b9f4d0d403b7

Hanging cage contraption by naaitsab, krzp & Taki17 + adds a faction for alternative animations to be used with OAR

dist/scripts/source/zadcFurnitureScript.psc
@@ -336,7 +336,14 @@ Function LockActor(actor act)
 	EndIf
 	ApplyEffects(user)
 	ApplyDevices(user)	
+	user.AddToFaction(clib.zadcNGInContraptionFaction)
 	user.SetFactionRank(clib.zadcNGInContraptionFaction, 1)
+	;50% chance the user gets placed in the altanim faction
+	;actors in the altanim faction will have different contraption idle animation
+	If ( Utility.RandomFloat( 0.0 , 49.9 ) < 50.0 )
+		user.AddToFaction( clib.zadcNGAltAnim )
+		user.SetFactionRank( clib.zadcNGAltAnim, 1 )
+	EndIf
 	RegisterForSingleUpdate(30)	
 	LastBreakEscapeAttemptAt = 0.0
 	LastStruggleEscapeAttemptAt = 0.0
@@ -386,7 +393,10 @@ Function UnlockActor()
 	RemoveEffects(user)
 	clib.ResetNiOverrideOverride(user)
 	RemoveDevices(user)
+	user.SetFactionRank(clib.zadcNGInContraptionFaction, 0)
 	user.RemoveFromFaction(clib.zadcNGInContraptionFaction)
+	user.SetFactionRank( clib.zadcNGAltAnim, 0 )
+	user.RemoveFromFaction( clib.zadcNGAltAnim )
 	;If ForceStripActor			
 	;	clib.RestoreOutfit(user)
 	;EndIf

dist/scripts/source/zadclibs.psc
@@ -23,6 +23,7 @@ Armor Property zadc_NoWaitItem Auto
 Armor Property zadc_PrisonerChainsInventory Auto
 Armor Property zadc_PrisonerChainsRendered Auto
 Faction Property zadcNGInContraptionFaction Auto
+Faction Property zadcNGAltAnim Auto
 
 Actor Property SelectedUser Auto Hidden
 
@@ -34,6 +35,7 @@ Activator Property zadc_gallowspole_strappado Auto
 Activator Property zadc_gallowspole_suspend_hogtie Auto
 Activator Property zadc_gallowspole_upside_down Auto
 Activator Property zadc_gallowspole_woodenhorse Auto
+Activator Property zadc_gibbet Auto
 Activator Property zadc_Pillory Auto
 Activator Property zadc_Pillory2 Auto
 Activator Property zadc_RestraintPost Auto
@@ -54,6 +56,7 @@ MiscObject Property zadc_kit_gallowspoleoverhead Auto
 MiscObject Property zadc_kit_gallowspolestrappado Auto
 MiscObject Property zadc_kit_gallowspoleupsidedown Auto
 MiscObject Property zadc_kit_gallowspolewoodenhorse Auto
+MiscObject Property zadc_kit_gibbet Auto
 MiscObject Property zadc_kit_pillory Auto
 MiscObject Property zadc_kit_pillory2 Auto
 MiscObject Property zadc_kit_restraintpost Auto

commit c0e90fa1aab17171176ac0ed1408afd9ef69d41b

Fix regression introduced in 09eee90
Commit 09eee90 reverted changes made by pull #46

dist/scripts/source/zadcFurnitureScript.psc
@@ -336,12 +336,10 @@ Function LockActor(actor act)
 	EndIf
 	ApplyEffects(user)
 	ApplyDevices(user)	
-	user.AddToFaction(clib.zadcNGInContraptionFaction)
 	user.SetFactionRank(clib.zadcNGInContraptionFaction, 1)
 	;50% chance the user gets placed in the altanim faction
 	;actors in the altanim faction will have different contraption idle animation
 	If ( Utility.RandomFloat( 0.0 , 99.0 ) < 50.0 )
-		user.AddToFaction( clib.zadcNGAltAnim )
 		user.SetFactionRank( clib.zadcNGAltAnim, 1 )
 	EndIf
 	RegisterForSingleUpdate(30)	
@@ -393,9 +391,7 @@ Function UnlockActor()
 	RemoveEffects(user)
 	clib.ResetNiOverrideOverride(user)
 	RemoveDevices(user)
-	user.SetFactionRank(clib.zadcNGInContraptionFaction, 0)
 	user.RemoveFromFaction(clib.zadcNGInContraptionFaction)
-	user.SetFactionRank( clib.zadcNGAltAnim, 0 )
 	user.RemoveFromFaction( clib.zadcNGAltAnim )
 	;If ForceStripActor			
 	;	clib.RestoreOutfit(user)